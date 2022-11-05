fb2d1eeecbe5c4554b51b3be6e0c389dbd91d3ba
Evgeniia <Kochetkovaev11@gmail.com>
2022-11-05 10:57:05 +0300

    fix: 1st review test2

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { errors } = require("celebrate");
-const router = require("./routes/index");
+const routes = require("./routes/index");
 const handleError = require("./middlewares/handle-error");
+
+const { PORT = 3000 } = process.env;
 
 mongoose
   .connect("mongodb://localhost:27017/mestodb", {
@@ -17,13 +19,12 @@ mongoose
     console.log("DB error", err);
   });
 
-const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(router);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(routes);
 app.use(errors());
 app.use(handleError);
 
